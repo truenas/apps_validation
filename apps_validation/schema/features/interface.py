47b0c17b227784e7475ca7b6c2822efe5f12029b
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 15:26:02 +0500

    Add interface related features

---
+++ apps_validation/schema/features/interface.py
@@ -0,0 +1,14 @@
+from apps_validation.schema.attrs import DictSchema, StringSchema
+
+from .base import BaseFeature
+
+
+class NormalizeInterfaceConfiguration(BaseFeature):
+    NAME = 'normalize/interfaceConfiguration'
+    VALID_SCHEMAS = [DictSchema]
+
+
+class DefinitionInterfaceFeature(BaseFeature):
+
+    NAME = 'definitions/interface'
+    VALID_SCHEMAS = [StringSchema]
