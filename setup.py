28c454c4ddd51f82f6bec332551a5628c006a343
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 14:36:07 +0500

    Add setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.1'
+
+setup(
+    name='apps_validation',
+    description='Validate TrueNAS Apps Catalog',
+    version=VERSION,
+    include_package_data=True,
+    packages=find_packages(),
+    license='GNU3',
+    platforms='any',
+    entry_points={
+        'console_scripts': [],
+    },
+)
