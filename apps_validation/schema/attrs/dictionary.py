06bbd70f3ec465353942376b7b7136610413854f
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 15:06:57 +0500

    Add object related schemas

--- apps_validation/schema/attrs/dictionary.py
+++ apps_validation/schema/attrs/dictionary.py
@@ -1,4 +1,30 @@
 from .base import BaseSchema
 
 
+class DictSchema(BaseSchema):
+    DEFAULT_TYPE = 'object'
 
+    def __init__(self, data):
+        self.attrs = []
+        self.additional_attrs = None
+        super().__init__(data=data)
+        self._skip_data_values = ['attrs']
+
+    def initialize_values(self, data):
+
+        super().initialize_values(data)
+        # self.attrs = [generate_variable(d) for d in (data.get('attrs') or [])]
+        # FIXME generate_variable is not defined in this file
+
+    def json_schema(self):
+        schema = super().json_schema()
+        schema['additionalProperties'] = bool(self.additional_attrs)
+        schema['properties']['attrs'] = {'type': 'array'}
+        schema['required'].append('attrs')
+        # We do not validate nested children and hence do not add it in the
+        # json schema as it makes it very complex to handle all the possibilities
+        return schema
+
+
+class CronSchema(BaseSchema):
+    DEFAULT_TYPE = 'object'
