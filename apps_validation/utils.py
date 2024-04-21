aa316850af639828e231ee5d5fe19151fa922f13
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 17:14:52 +0500

    Specify cached files names

--- apps_validation/utils.py
+++ apps_validation/utils.py
@@ -1,7 +1,5 @@
 import re
 
 
-CACHED_CATALOG_FILE_NAME = 'catalog.json'
-CACHED_VERSION_FILE_NAME = 'app_versions.json'
 RE_VERSION_PATTERN = re.compile(r'(\d{2}\.\d{2}(?:\.\d)*)')  # We are only interested in XX.XX here
 WANTED_FILES_IN_ITEM_VERSION = {'questions.yaml', 'app-readme.md', 'Chart.yaml', 'README.md'}  # FIXME
