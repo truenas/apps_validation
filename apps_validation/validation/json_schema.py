c2eff00c5ee498d7123c8f94c25f93bba6417b5b
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 18:02:26 +0500

    Add method to validate app renaming migrations

--- apps_validation/validation/json_schema.py
+++ apps_validation/validation/json_schema.py
@@ -1,3 +1,38 @@
+APP_MIGRATION_SCHEMA = {
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'app_name': {'type': 'string'},
+            'action': {'type': 'string', 'enum': ['move']},
+        },
+        'required': [
+            'app_name',
+            'action'
+        ],
+        'allOf': [
+            {
+                'if': {
+                    'properties': {
+                        'action': {
+                            'const': 'move',
+                        },
+                    },
+                },
+                'then': {
+                    'properties': {
+                        'old_train': {'type': 'string'},
+                        'new_train': {'type': 'string'},
+                    },
+                    'required': [
+                        'new_train',
+                        'old_train',
+                    ],
+                },
+            },
+        ],
+    },
+}
 CATALOG_JSON_SCHEMA = {
     'type': 'object',
     'patternProperties': {
---
+++ apps_validation/validation/validate_app_rename_migrations.py
@@ -0,0 +1,34 @@
+import json
+import jsonschema
+import os
+import re
+
+from apps_validation.exceptions import ValidationErrors
+
+from .json_schema import APP_MIGRATION_SCHEMA
+
+
+MIGRATION_NAME_STR = r'^\d+\w+.json'
+RE_MIGRATION_NAME = re.compile(MIGRATION_NAME_STR)
+
+
+def validate_migrations(migration_dir):
+    verrors = ValidationErrors()
+    for migration_file in os.listdir(migration_dir):
+        if not RE_MIGRATION_NAME.findall(migration_file):
+            verrors.add(
+                f'app_migrations.{migration_file}',
+                'Invalid naming scheme used for migration file name. '
+                f'It should be conforming to {MIGRATION_NAME_STR!r} pattern.'
+            )
+        else:
+            try:
+                with open(os.path.join(migration_dir, migration_file), 'r') as f:
+                    data = json.loads(f.read())
+                jsonschema.validate(data, APP_MIGRATION_SCHEMA)
+            except (json.JSONDecodeError, jsonschema.ValidationError) as e:
+                verrors.add(
+                    f'app_migrations.{migration_file}',
+                    f'Failed to validate migration file structure: {e}'
+                )
+    verrors.check()
