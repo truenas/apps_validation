5bd91cab9bfe2bc6c634dc5766990f22d33da410
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 15:27:22 +0500

    Add basic features

---
+++ apps_validation/schema/features/basic.py
@@ -0,0 +1,15 @@
+from apps_validation.schema.attrs import DictSchema, StringSchema
+
+from .base import BaseFeature
+
+
+class DefinitionGPUConfigurationFeature(BaseFeature):
+
+    NAME = 'definitions/gpuConfiguration'
+    VALID_SCHEMAS = [DictSchema]
+
+
+class DefinitionTimezoneFeature(BaseFeature):
+
+    NAME = 'definitions/timezone'
+    VALID_SCHEMAS = [StringSchema]
