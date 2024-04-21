71927a30548aa1d3108904308627539bcc69d0c8
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 15:18:00 +0500

    Add base features class

---
+++ apps_validation/schema/features/base.py
@@ -0,0 +1,28 @@
+from apps_validation.exceptions import ValidationErrors
+
+
+class BaseFeature:
+
+    NAME = NotImplementedError
+    VALID_SCHEMAS = []
+
+    def __str__(self):
+        return self.NAME
+
+    def validate(self, schema_obj, schema_str):
+        verrors = ValidationErrors()
+        if not isinstance(schema_obj, tuple(self.VALID_SCHEMAS)):
+            verrors.add(
+                f'{schema_str}.type',
+                f'Schema must be one of {", ".join(str(v) for v in self.VALID_SCHEMAS)} schema types'
+            )
+
+        if not verrors:
+            self._validate(verrors, schema_obj, schema_str)
+        verrors.check()
+
+    def _validate(self, verrors, schema_obj, schema_str):
+        pass
+
+    def __eq__(self, other):
+        return self.NAME == (other if isinstance(other, str) else other.NAME)
