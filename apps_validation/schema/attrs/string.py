5bf84dbd79266b66fa186d1931921dceb806de97
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-21 15:02:55 +0500

    Add text field schema

--- apps_validation/schema/attrs/string.py
+++ apps_validation/schema/attrs/string.py
@@ -28,3 +28,23 @@ class StringSchema(Schema):
             },
         })
         return schema
+
+
+class TextFieldSchema(StringSchema):
+    def __init__(self, data):
+        super().__init__(data)
+        self.max_length = 1024 * 1024
+
+    def json_schema(self):
+        schema = super().json_schema()
+        schema['properties'].update({
+            'max_length': {
+                'type': 'integer',
+                'const': 1024 * 1024,
+            },
+            'language': {
+                'type': 'string',
+                'enum': ['yaml', 'json', 'toml', 'text'],
+            }
+        })
+        return schema
