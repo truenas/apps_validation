2d8558314c11a59be5d2a3578ae4f201f4adf09f
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-06 01:57:45 +0500

    Add a util to read provided values file

--- catalog_reader/app_utils.py
+++ catalog_reader/app_utils.py
@@ -44,8 +44,17 @@ def get_default_questions_context() -> dict:
 
 def get_app_basic_details(app_path: str) -> dict:
     # This just retrieves app name and app version from app path
-    with contextlib.suppress(FileNotFoundError, yaml.YAMLError):
-        app_config = yaml.safe_load(open(os.path.join(app_path, 'app.yaml')))
+    with contextlib.suppress(FileNotFoundError, yaml.YAMLError, KeyError):
+        with open(os.path.join(app_path, 'app.yaml'), 'r') as f:
+            app_config = yaml.safe_load(f.read())
         return {k: app_config[k] for k in ('name', 'train', 'version', 'lib_version')}
+
+    return {}
+
+
+def get_values(values_path: str) -> dict:
+    with contextlib.suppress(FileNotFoundError, yaml.YAMLError):
+        with open(values_path, 'r') as f:
+            return yaml.safe_load(f.read())
 
     return {}
