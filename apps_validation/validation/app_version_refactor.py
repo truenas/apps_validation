1801cae949d6332bf0a8aeaf81eba403610906ae
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-07 00:01:54 +0500

    Validate app config via defined json schema

--- apps_validation/validation/app_version_refactor.py
+++ apps_validation/validation/app_version_refactor.py
@@ -3,7 +3,11 @@ import yaml
 
 from typing import Optional
 
+from jsonschema import validate as json_schema_validate, ValidationError as JsonValidationError
+
 from apps_validation.exceptions import ValidationErrors
+
+from .json_schema_utils import APP_METADATA_JSON_SCHEMA
 
 
 def validate_app_version_file(
@@ -22,3 +26,9 @@ def validate_app_version_file(
         except yaml.YAMLError:
             verrors.add(schema, 'Must be a valid yaml file')
             return verrors
+
+    try:
+        json_schema_validate(app_config, APP_METADATA_JSON_SCHEMA)
+    except JsonValidationError as e:
+        verrors.add(schema, f'Failed to validate app version file: {e.message}')
+        return verrors
