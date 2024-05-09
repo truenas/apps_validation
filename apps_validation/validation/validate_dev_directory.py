8fcaf17c2d906a4d441626c20c51cc5491c13289
caleb <yocalebo@gmail.com>
2024-05-09 10:46:41 -0400

    fix validate_dev_directory.py

--- apps_validation/validation/validate_dev_directory.py
+++ apps_validation/validation/validate_dev_directory.py
@@ -1,6 +1,7 @@
+import pathlib
 import os
-import yaml
 
+import yaml
 from jsonschema import ValidationError as JsonValidationError
 
 from apps_validation.exceptions import ValidationErrors
@@ -16,53 +17,53 @@ from .validate_app_version import validate_catalog_item_version
 
 def validate_dev_directory_structure(catalog_path: str, to_check_apps: dict) -> None:
     verrors = ValidationErrors()
-    dev_directory = get_ci_development_directory(catalog_path)
-    if not os.path.exists(dev_directory):
+    try:
+        for i in pathlib.Path(get_ci_development_directory(catalog_path)).iterdir():
+            if i.is_dir() and i.name in to_check_apps:
+                validate_train(catalog_path, i.as_posix(), f'dev.{i.name}', to_check_apps[i.name])
+    except (FileNotFoundError, NotADirectoryError):
         return
 
-    for train_name in filter(
-        lambda name: name in to_check_apps and os.path.isdir(os.path.join(dev_directory, name)),
-        os.listdir(dev_directory)
-    ):
-        validate_train(
-            catalog_path, os.path.join(dev_directory, train_name), f'dev.{train_name}', to_check_apps[train_name]
-        )
     verrors.check()
 
 
 def validate_train(catalog_path: str, train_path: str, schema: str, to_check_apps: list) -> None:
     verrors = ValidationErrors()
     train_name = os.path.basename(train_path)
-    for app_name in filter(
-        lambda name: os.path.isdir(os.path.join(train_path, name)), os.listdir(train_path)
-    ):
+    for app_path in pathlib.Path(train_path).iterdir():
+        if not app_path.is_dir():
+            continue
+
+        app_name = app_path.name
         if app_name not in to_check_apps:
             continue
 
-        app_path = os.path.join(train_path, app_name)
         try:
-            validate_app(app_path, f'{schema}.{app_name}', train_name)
+            validate_app(app_path.as_posix(), f'{schema}.{app_name}', train_name)
         except ValidationErrors as ve:
             verrors.extend(ve)
         else:
             published_train_app_path = os.path.join(catalog_path, train_name, app_name)
-            if not os.path.exists(published_train_app_path):
+            try:
+                if not version_has_been_bumped(published_train_app_path, get_app_version(app_path)):
+                    verrors.add(
+                        f'{schema}.{app_name}.version',
+                        'Version must be bumped as app has been changed but version has not been updated'
+                    )
+            except FileNotFoundError:
                 # The application is new and we are good
                 continue
-
-            if not version_has_been_bumped(published_train_app_path, get_app_version(app_path)):
-                verrors.add(
-                    f'{schema}.{app_name}.version',
-                    'Version must be bumped as app has been changed but version has not been updated'
-                )
 
         verrors.check()
 
 
 def validate_upgrade_strategy(app_path: str, schema: str, verrors: ValidationErrors):
     upgrade_strategy_path = os.path.join(app_path, UPGRADE_STRATEGY_FILENAME)
-    if os.path.exists(upgrade_strategy_path) and not os.access(upgrade_strategy_path, os.X_OK):
-        verrors.add(schema, f'{upgrade_strategy_path!r} is not executable')
+    try:
+        if not os.access(upgrade_strategy_path, os.X_OK):
+            verrors.add(schema, f'{upgrade_strategy_path!r} is not executable')
+    except FileNotFoundError:
+        pass
 
 
 def validate_app(app_dir_path: str, schema: str, train_name: str) -> None:
@@ -76,14 +77,19 @@ def validate_app(app_dir_path: str, schema: str, train_name: str) -> None:
         app_dir_path, schema, get_app_version(app_dir_path), app_name, True, train_name=train_name,
     )
 
-    required_files = set(REQUIRED_METADATA_FILES)
-    available_files = set(
-        f for f in filter(lambda f: os.path.isfile(os.path.join(app_dir_path, f)), os.listdir(app_dir_path))
-    )
-    if missing_files := required_files - available_files:
+    # TODO: Stop using globally defined mutable objects
+    total_num_of_required_files = len(REQUIRED_METADATA_FILES)
+    total_num_of_required_files_found = 0
+    for i in pathlib.Path(app_dir_path).iterdir():
+        if total_num_of_required_files_found == total_num_of_required_files:
+            break
+        elif i.is_file() and i.name in REQUIRED_METADATA_FILES:
+            total_num_of_required_files_found += 1
+
+    if total_num_of_required_files_found != total_num_of_required_files:
         verrors.add(
             f'{schema}.required_files',
-            f'{", ".join(missing_files)!r} file(s) must be specified'
+            f'All of the following files must exist: {", ".join(REQUIRED_METADATA_FILES)!r}'
         )
     validate_upgrade_strategy(app_dir_path, f'{schema}.{UPGRADE_STRATEGY_FILENAME}', verrors)
     verrors.check()
