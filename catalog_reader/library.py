432577aa3a4c5f373f04346ba07c5db0e615ee6e
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-10 03:55:28 +0500

    Properly generate hashes of libs

--- catalog_reader/library.py
+++ catalog_reader/library.py
@@ -21,12 +21,13 @@ def get_library_hashes(library_path: str) -> dict:
 
 
 def get_hashes_of_base_lib_versions(catalog_path: str) -> dict:
-    library_dir = pathlib.Path(get_library_path(catalog_path))
+    library_path = get_library_path(catalog_path)
+    library_dir = pathlib.Path(library_path)
     hashes = {}
     for lib_entry in library_dir.iterdir():
         if not lib_entry.is_dir() or not RE_VERSION.match(lib_entry.name):
             continue
 
-        hashes[lib_entry.name] = get_hash_of_directory(os.path.join(library_dir.name, lib_entry.name))
+        hashes[lib_entry.name] = get_hash_of_directory(os.path.join(library_path, lib_entry.name))
 
     return hashes
