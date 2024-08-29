569aa2a4e298844658ded1994830ccd1d669a905
Stavros kois <s.kois@outlook.com>
2024-08-29 15:02:09 +0300

    pull fixes from `bump-script` branch

--- apps_ci/version_bump.py
+++ apps_ci/version_bump.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from apps_exceptions import ValidationErrors
+from apps_exceptions import AppDoesNotExist, ValidationErrors
 
 
 def is_valid_version(version: str) -> bool:
@@ -29,11 +29,10 @@ def rename_versioned_dir(version: str, new_version: str, train_name: str, app_di
 
     if not is_valid_version(version) or not is_valid_version(new_version):
         verrors.add('version_bump', f'Invalid version {version!r} or {new_version!r}')
+    verrors.check()
 
     if not app_dir.is_dir():
-        verrors.add('version_bump', f'{app_dir.name!r} is not a directory')
-
-    verrors.check()
+        raise AppDoesNotExist(app_dir)
 
     dir_base = app_dir / 'templates/library' / train_name / app_dir.name
     curr_versioned_dir = dir_base / f'v{version.replace(".", "_")}'
@@ -43,6 +42,6 @@ def rename_versioned_dir(version: str, new_version: str, train_name: str, app_di
 
     if new_versioned_dir.is_dir():
         verrors.add('version_bump', f'App {app_dir.name!r} library with version {new_version!r} already exists')
-    curr_versioned_dir.rename(new_versioned_dir)
+    verrors.check()
 
-    print(f'[\033[92mOK\x1B[0m]\tUpdated app {app_dir.name!r} library from {version!r} to {new_version!r}')
+    curr_versioned_dir.rename(new_versioned_dir)
