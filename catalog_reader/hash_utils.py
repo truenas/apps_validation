7c07f001aa57abbad50791c9a25eee94ce6b7516
Stavros Kois <s.kois@outlook.com>
2025-05-15 15:44:06 +0300

    remove extra quote

--- catalog_reader/hash_utils.py
+++ catalog_reader/hash_utils.py
@@ -6,7 +6,7 @@ def get_hash_of_directory(directory: str, dir_exclude_patters: list[str] | None 
     This returns sha256sum of the directory
     """
     dir_exclude_patters = dir_exclude_patters or []
-    find_command = f'find {directory} -type f"'
+    find_command = f'find {directory} -type f'
     for pattern in dir_exclude_patters:
         find_command += f' -not -path "{directory}/**/{pattern}/*"'
 
