397f93932cc0013bce31e2487b9287f1dde9e620
Stavros Kois <s.kois@outlook.com>
2025-05-15 15:44:21 +0300

    its not an f-string

--- catalog_reader/hash_utils.py
+++ catalog_reader/hash_utils.py
@@ -10,7 +10,7 @@ def get_hash_of_directory(directory: str, dir_exclude_patters: list[str] | None 
     for pattern in dir_exclude_patters:
         find_command += f' -not -path "{directory}/**/{pattern}/*"'
 
-    find_command += ' -exec sha256sum {{}} + | sort | awk \'{{print $1}}\' | sha256sum'
+    find_command += ' -exec sha256sum {} + | sort | awk \'{print $1}\' | sha256sum'
 
     cp = subprocess.run(find_command, capture_output=True, check=True, shell=True)
     return cp.stdout.decode().split()[0]
